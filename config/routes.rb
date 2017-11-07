eceb85df93e2a19621259bb2c33cb4cd4013093c
Nathalia de Luccas Filopoulos <nathiluccas@gmail.com>
2017-11-07 15:06:46 +0000

    add task CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  get "tasks", to: "tasks#index"
+  # get "tasks", to: "tasks#index"
+  # get "tasks/:id", to: "tasks#show"
+  # get "tasks/new", to: "tasks#new"
+  # post "tasks", to: "tasks#create"
+  # get "tasks/:id/edit", to: "tasks#edit"
+  # patch "tasks/:id", to: "tasks#update"
+  # delete "tasks/:id", to: "tasks#destroy"
+   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
